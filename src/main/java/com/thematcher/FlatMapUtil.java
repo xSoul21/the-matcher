6dc1ab3f686b58d8ea1d61f212af6683c1f134af
Fabrizio Catapano <catapan2@ITLAA0B0D5.lan>
2024-02-26 23:10:54 +0100

    adding element

--- src/main/java/com/thematcher/FlatMapUtil.java
+++ src/main/java/com/thematcher/FlatMapUtil.java
@@ -22,12 +22,12 @@ public final class FlatMapUtil {
         if (entry == null) {
             return Stream.empty();
         }
-
+        // caso in cui si una oggetto con altri oggetti "base" chiave/valore
         if (entry.getValue() instanceof Map<?, ?>) {
             return ((Map<?, ?>) entry.getValue()).entrySet().stream()
                     .flatMap(e -> flatten(new AbstractMap.SimpleEntry<>(entry.getKey() + "/" + e.getKey(), e.getValue())));
         }
-
+        // caso inb cui sia un array "key":[]
         if (entry.getValue() instanceof List<?>) {
             List<?> list = (List<?>) entry.getValue();
             return IntStream.range(0, list.size())
@@ -35,7 +35,7 @@ public final class FlatMapUtil {
                     .mapToObj(i -> new AbstractMap.SimpleEntry<String, Object>(entry.getKey() + "/x/" + ((HashMap)list.get(i)).values().toString(), list.get(i)))
                     .flatMap(FlatMapUtil::flatten);
         }
-
+        // caso base, e' un valore primitivo
         return Stream.of(entry);
     }
 }
